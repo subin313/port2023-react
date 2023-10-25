9bc2987fa1adf95671c45aaad140970899f8af8d
subin313 <82709790+subin313@users.noreply.github.com>
2023-10-25 16:01:52 +0900

    😱 인트로 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 
-const header__Nav = [
+const headerNav = [
     {
         title:"intro",
         url:"#intro",
@@ -28,16 +28,17 @@ const Header = () => {
 
     const toggleMenu = () => {
         setShow((prevShow) => !prevShow);
-    }
+    };
+
     return(
         <header id="header" role="banner">
                 <div className="header__inner">
                     <div className="header__logo">
-                        <a href="/">portfolio<em>vite</em></a>
+                        <a href="/">portfolio<em>react.js</em></a>
                     </div>
-                    <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+                    <nav className={`header__nav ${show ?"show":""}`} role="navigation" aria-label="메인 메뉴">
                         <ul>
-                        {header__Nav.map((nav,key)=>(
+                        {headerNav.map((nav,key)=>(
                             <li key={key}>
                                 <a href={nav.url}>{nav.title}</a>
                             </li>
@@ -48,7 +49,7 @@ const Header = () => {
                         className="header__nav__mobile"
                         id="headerToggle"
                         aria-controls="primary-menu"
-                        aria-expanded={show?"true":"false"}
+                        aria-expanded={show ? "true":"false"}
                         role="button"
                         tabIndex="0"
                         onClick={toggleMenu}
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -5,6 +5,7 @@ import Skill from "../components/Skill";
 import Port from "../components/Port";
 import Footer from "../components/Footer";
 import Skip from "../components/Skip";
+import Site from "../components/Site";
 import Main from "../components/Main";
 import Contact from "../components/Contact";
 
