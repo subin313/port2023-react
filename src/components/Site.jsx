ac425fafe281d026f172bd8c0c1d834fd5f39204
subin313 <82709790+subin313@users.noreply.github.com>
2023-10-26 17:11:34 +0900

    🧐 포트폴리오 완료

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,83 +1,80 @@
 import React from "react";
 
+const siteText =[
+    {
+    text:["make","site compliant with","webstandard"],
+    title:"비트를 이용한 사이트 제작",
+    code:"https://github.com/subin313/port2023-vite.git",
+    view:"https://2023-subin-vite.netlify.app",
+    info:[
+        "site coding",
+        "production period : two days",
+        "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+},
+{
+    text:["make","site compliant with","webstandard"],
+    title:"리액트를 이용한 사이트 제작",
+    code:"https://github.com/subin313/port2023-vite.git",
+    view:"https://2023-subin-vite.netlify.app",
+    info: [
+        "site coding",
+        "production period : two days",
+        "use stack : HTML5/CSS3, CSS Variable, Vite",
+    ],
+},
+{
+    text:["make","site compliant with","vue.js"],
+    title:"뷰를 이용한 사이트 제작",
+    code:"https://github.com/subin313/port2023-vite",
+    view:"http://2023-subin-vite.netlify.app",
+    info:[
+        "site coding",
+        "production period : two days",
+        "use stack : HTML5/CSS3, CSS Variable, vue",
+        ],
+},
+{
+    text:["make","site compliant with","next.js"],
+    title:"넥스트를 이용한 사이트 제작",
+    code:"https://github.com/subin313/port2023-vite",
+    view:"http://2023-subin-vite.netlify.app",
+    info:[
+        "site coding",
+        "production period : two days",
+        "use stack : HTML5/CSS3, Scss Vaiable, next.js",
+    ],
+},
+];
+
 const Site = () =>{
     return (
     <section id="site">    
         <div className="site__inner">
             <h2 className="site__title">Site coding<em>나의 작업물</em></h2>
             <div className="site__wrap">
-                <article className="site__item s1">
-                    <span className="num">1.</span>
+                {siteText.map((site,key)=>(
+                <article className={`site__item s${key+1}`}key={key}>
+                    <span className="num">{key+1}.</span>
                     <div className="text">
-                        <div>Make</div>
-                        <div>site compliant with</div>
-                        <div>webstandard</div>
+                        <div>{site.text[0]}</div>
+                        <div>{site.text[1]}</div>
+                        <div>{site.text[2]}</div>
                     </div>
-                    <h3 className="title">웹 표준을 준수한 사이트 제작</h3>
+                    <h3 className="title">
+                        {site.title}
+                    </h3>
                     <div className="btn">
-                        <a href="/">code</a>
-                        <a href="/">view</a>
+                        <a href={site.code}>code</a>
+                        <a href={site.view}>view</a>
                     </div>
                     <div className="info">
-                        <span>site coding</span>
-                        <span>production period : two days</span>
-                        <span>use stack : html5/css3, CSS Variable, Vite</span>
+                        <span>{site.info[0]}</span>
+                        <span>{site.info[1]}</span>
+                        <span>{site.info[2]}</span>
                     </div>
                 </article>
-                <article className="site__item s2">
-                    <span className="num">2.</span>
-                    <div className="text">
-                        <div>Make</div>
-                        <div>site compliant with</div>
-                        <div>react.js</div>
-                    </div>
-                    <h3 className="title">리엑트를 이용한 사이트 제작</h3>
-                    <div className="btn">
-                        <a href="/">code</a>
-                        <a href="/">view</a>
-                    </div>
-                    <div className="info">
-                        <span>site coding</span>
-                        <span>production period : two days</span>
-                        <span>use stack : html5/css3, CSS Variable, Vite</span>
-                    </div>
-                </article>
-                <article className="site__item s3">
-                    <span className="num">3.</span>
-                    <div className="text">
-                        <div>Make</div>
-                        <div>site compliant with</div>
-                        <div>vue.js</div>
-                    </div>
-                    <h3 className="title">뷰를 이용한 사이트 제작</h3>
-                    <div className="btn">
-                        <a href="/">code</a>
-                        <a href="/">view</a>
-                    </div>
-                    <div className="info">
-                        <span>site coding</span>
-                        <span>production period : two days</span>
-                        <span>use stack : vue.js, CSS Variable, Vite</span>
-                    </div>
-                </article>
-                <article className="site__item s4">
-                    <span className="num">4.</span>
-                    <div className="text">
-                        <div>Make</div>
-                        <div>site compliant with</div>
-                        <div>vue.js</div>
-                    </div>
-                    <h3 className="title">넥스트를 이용한 사이트 제작</h3>
-                    <div className="btn">
-                        <a href="/">code</a>
-                        <a href="/">view</a>
-                    </div>
-                    <div className="info">
-                        <span>site coding</span>
-                        <span>production period : two days</span>
-                        <span>use stack : nest.js, CSS Variable, Vite</span>
-                    </div>
-                </article>
+                ))}
             </div>
         </div>
     </section>
